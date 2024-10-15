a7c68da9bc32b9ed21c908a0511028cae3df44f7
AudreyMl <audreytraining33@gmail.com>
2024-10-15 10:15:57 +0200

    pages et fichiers json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Notfound from './Pages/Not-found';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const app = (
+  <BrowserRouter>
+    <div className='main-container'>
+      <Header />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/Not-found" element={<Notfound />} />
+          </Routes>
+        </Router>
+      </main>
+      <Footer />
+
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
