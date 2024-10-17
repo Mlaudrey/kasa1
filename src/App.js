5bf5e9667961335e6df7d94c5348fd2cb4e0f1b1
AudreyMl <audreytraining33@gmail.com>
2024-10-17 10:12:44 +0200

    babel et images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import react from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter,Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
