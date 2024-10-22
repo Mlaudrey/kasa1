e302ac0b49ff4a95d9cfd64c7ff59d38a1e282ae
AudreyMl <audreytraining33@gmail.com>
2024-10-22 23:16:41 +0200

    construction de cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Card.scss'; 
 
-const Card = ({ id, title, image }) => {
-  return (
-    <div className="cards">
-      <Link to={`/Fiche-Logement/${id}`} className="cards-link">
-        <img src={image} alt={title} className="cards-img" />
-        <h3 className="cards-title">{title}</h3>
-      </Link>
-      <p>ID: {id}</p>
-    </div>
-  );
+const Card = ({ id, title, pictures }) => {
+    return (
+        <div className="cards">
+            <Link to={`/Fiche-Logement/${id}`} className="card-link">
+                <img src={pictures && pictures.length > 0 ? pictures[0] : 'chemin/vers/image/default.jpg'} alt={title} className="cards-img" />
+                <h3 className="card-title">{title}</h3>
+            </Link>
+        </div>
+    );
 };
 
 export default Card;
--- src/Pages/Fiche-Logement/Fiche-Logement.js
+++ src/Pages/Fiche-Logement/Fiche-Logement.js
@@ -1,2 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './Fiche-Logement.scss';
+import data from '../../Data/logement.json'; 
+
+const FicheLogement = () => {
+  const { id } = useParams(); // récupère l'ID depuis l'URL
+  const [logement, setLogement] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // recherche le logement correspondant dans les données importées
+    const logementData = data.find(logement => logement.id === id);
+
+    if (logementData) {
+      setLogement(logementData);
+      setLoading(false); // indique que le chargement est terminé
+    } else {
+      setError("Logement non trouvé.");
+      setLoading(false);
+    }
+  }, [id]);
+
+  if (loading) return <div>Chargement...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      {logement ? (
+        <div>
+          <h1>{logement.titre}</h1>
+          <p>{logement.description}</p>
+        </div>
+      ) : (
+        <p>Aucun logement trouvé.</p>
+      )}
+    </div>
+  );
+};
+
+export default FicheLogement;
