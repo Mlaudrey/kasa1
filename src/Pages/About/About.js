8f402f1bf5c75f52a3af666b3f727870f5582688
AudreyMl <audreytraining33@gmail.com>
2024-10-25 00:24:32 +0200

    description collapse

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,13 +7,31 @@ import Collapse from '../../Components/Collapse/Collapse';
 const About = () => {
   return (
     <>
-      <Banner img={AboutBanner} />
+        <Banner img={AboutBanner} />
       
-      <Collapse
-        type=""
-        title="Fiabilité"
-        description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-      />
+        <Collapse
+            type=""
+            title="Fiabilité"
+            description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        />
+
+        <Collapse
+            type=""
+            title="Respect"
+            description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+
+        <Collapse
+            type=""
+            title="Service"
+            description="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+        />
+
+        <Collapse
+            type=""
+            title="Sécurité"
+            description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
     </>
   );
 };
