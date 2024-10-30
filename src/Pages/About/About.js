f695f9834bff2a34e84359ba8afdf9816c2abd16
AudreyMl <audreytraining33@gmail.com>
2024-10-30 21:54:23 +0100

    media queries

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,7 +6,7 @@ import Collapse from '../../Components/Collapse/Collapse';
 
 const About = () => {
   return (
-    <>
+    <div className="about-container">
         <Banner img={AboutBanner} />
       
         <Collapse
@@ -32,7 +32,7 @@ const About = () => {
             title="Sécurité"
             description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         />
-    </>
+    </div>
   );
 };
 
